1f92a286b25be1b3c3f5c63d64de463688dc6256
tommoyang <tommoyang@hotmail.com>
2017-04-10 02:19:11 +1200

    Initial Commit

---
+++ src/RecursiveReverseString.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+/**
+ * https://www.codewars.com/kata/recursive-reverse-string
+ */
+
+public class RecursiveReverseString {
+
+    public static void main(String[] args) {
+        System.out.println(reverse("Reverse This!"));
+    }
+
+    /**
+     * Use the debugger to watch what happens to a string as this function recurses
+     */
+    public static String reverse(String str) {
+        // Put a break point on this line - click the empty space next to the line number, and a red dot should appear
+        // Then run the class by pressing the "bug" button instead of the "play" button
+        // Have a play with the buttons you get in the new debug window below - there are too many to describe easily
+        char c = str.charAt(0);
+
+        String n = str.substring(1, str.length());
+        if (str.length() > 1) {
+            n = reverse(n);
+        }
+
+        return n + c;
+    }
+}
---
+++ src/Sandbox.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* Before you do anything, note that you can do everything with just the keyboard!
+* Try to resist the urge to click on something unless explicitly told to
+*/
+
+/**
+* Generic shortcuts that may be useful:
+* Copy     Ctrl + C
+* Paste    Ctrl + V
+* Undo     Ctrl + Z
+*
+* Select text - Hold down Shift and press left/right/up/down
+* Navigate by "blocks" - Hold down Ctrl and press left/right
+* (Try combining them)
+*/
+
+/**
+* To begin with, press Ctrl+Alt+L to reindent the page
+* (Easy way to not lose too many marks for poor formatting)
+*/
+
+public class Sandbox {
+/**
+* Make a main method - Type "psvm" below, and then press tab
+* There are other versions of this - try Ctrl+J and looking around
+*/
+
+/**
+* Try using Alt+Enter on gray, yellow-highlighted, red and red-underlined statements.
+*/
+private void needsSomeFixing() {
+if (true) {
+System.out.println("This will always print");}
+
+randomList = new ArrayList<String>();
+
+List<String> otherList = getNewList(15);
+}
+
+public static List<String> getNewList() {
+// I don't actually need to do anything with the argument, by the way
+return new ArrayList<>();
+}
+
+/**
+* Try pressing Ctrl and hovering, Ctrl + Click or Alt + F7 on the "getNewList" part
+*/
+private static void doSomethingElse() {
+List<String> list = Sandbox.getNewList();
+}
+
+/**
+* These need getters and setters. You can either Alt+Enter or Alt+Insert to create them.
+* Try creating a constructor with Alt+Enter too!
+*/
+private int intField;
+private String stringField;
+
+/**
+* Once you've made a constructor, press Shift+F6 on the variable names. This should let you rename all of
+* the instances of this variable, without needing to find and replace everything
+*/
+
+/**
+* You're done with this part.
+* Press Ctrl+Shift+N (File search) and look for "HtmlSandbox.html"
+* You can also do this with Ctrl+N (Java Classes only)
+*/
+}
